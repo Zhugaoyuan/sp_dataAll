7c07b02d27581ff961f1df9dfed2153111cbac55
Administrator <Administrator@PC201906051206>
2019-07-08 15:38:52 +0800

    11

---
+++ 7.17Template1/Controllers/ListPageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _7._17Template1.Controllers
+{
+    public class ListPageController : Controller
+    {
+        // GET: ListPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
